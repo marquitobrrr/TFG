64872bba8d8726056923084bc9ea273384bdd199
marquitobrrr <mmrivero13@gmail.com>
2025-04-12 13:09:58 +0200

    code change

--- Servijam/db.php
+++ Servijam/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "print_server";
 $username = "root";
-$password = ""; // poné tu contraseña aquí
+$password = "232425"; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
